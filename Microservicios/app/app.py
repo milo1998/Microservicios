a090b0b6efd9414c6e039838db2b90574e2417dd
ndmc98 <norbey.munoz98@gmail.com>
2019-09-22 19:11:41 -0500

    Update app.py

--- Microservicios/app/app.py
+++ Microservicios/app/app.py
@@ -1,7 +1,3 @@
-# compose_flask/app.py
-# https://runnable.com/docker/python/docker-compose-with-flask-apps
-# https://stackoverflow.com/questions/42316511/communication-between-two-flask-services-in-docker
-# https://github.com/eelkevdbos/microservices-flask/blob/master/concat/concat.py
 from flask import Flask
 from flask import render_template,request,make_response
 import requests
